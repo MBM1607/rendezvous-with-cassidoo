fc84b3d00aeabe3f257ef5572885edc0601d24d2
mbm_1607 <muhammadkhan1607@gmail.com>
2022-08-09 16:09:48 +0500

    Add TODO & comment to issue# 260

--- src/260-swap-pairs/swap-pairs.ts
+++ src/260-swap-pairs/swap-pairs.ts
@@ -1,3 +1,17 @@
+/*
+Given a list, swap every two adjacent nodes.
+Something to think about: How would your code change if this were a linked list, versus an array ?
+
+Example:
+
+```js
+> swapPairs([1,2,3,4])
+> [2,1,4,3]
+
+> swapPairs([])
+> []
+```
+*/
 const swapArrayPairs = (array: Array<number>): Array<number> => {
   const swapped: Array<number> = [];
 
@@ -8,5 +22,7 @@ const swapArrayPairs = (array: Array<number>): Array<number> => {
 
   return swapped;
 };
+
+// TODO Add linked list solution
 
 export { swapArrayPairs };
