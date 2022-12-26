291e1ef5ce42528525a27aad059739b3c4c2d195
mbm_1607 <muhammadkhan1607@gmail.com>
2022-12-26 13:30:40 +0500

    Add one extra test case for issue# 279

--- src/279-capital-after-vowel/capital-after-vowel.test.ts
+++ src/279-capital-after-vowel/capital-after-vowel.test.ts
@@ -18,6 +18,24 @@ const capitalAfterVowelTests: capitalAfterVowelTest[] = [
     input: 'A quick brown fox jumps over the lazy dog',
     output: 'A QuiCk broWn foX juMps oVeR the LaZy DoG',
   },
+  {
+    input: `
+      All that is gold does not glitter,
+      Not all those who wander are lost;
+      The old that is strong does not wither,
+      Deep roots are not reached by the frost.
+      From the ashes a fire shall be woken,
+      A light from the shadows shall spring;
+    `,
+    output: `
+      ALl thaT iS goLd doeS noT gliTteR,
+      NoT aLl thoSe Who WaNdeR aRe LoSt;
+      The oLd thaT iS stroNg doeS noT wiTheR,
+      DeeP rooTs aRe NoT reaCheD by The FroSt.
+      FroM the aSheS a FiRe ShaLl be WoKeN,
+      A LiGht froM the ShaDoWs shaLl spriNg;
+    `,
+  },
 ];
 
 describe('testing capitalAfterVowel', () => {
