3d89f4abf988d2692bad5f5c8bd571f5b9991fd2
mbm_1607 <muhammadkhan1607@gmail.com>
2023-04-02 00:09:32 +0500

    :sparkling_heart: Solve 293-roll-dice

--- src/293-roll-dice/roll-dice.test.ts
+++ src/293-roll-dice/roll-dice.test.ts
@@ -1,4 +1,4 @@
-import { rollDice } from "./roll-dice";
+import { rollDice, rollSingleDice } from "./roll-dice";
 
 interface RollDiceTest {
   input: string;
@@ -22,11 +22,30 @@ const ROLL_DICE_TESTS: RollDiceTest[] = [
 
 describe("rollDice", () => {
   test("it should roll dice", () => {
-    ROLL_DICE_TESTS.forEach(({ input, output_range }) => {
-      const [low_limit, high_limit] = output_range;
-      const output = rollDice(input);
-      expect(output).toBeGreaterThanOrEqual(low_limit);
-      expect(output).toBeLessThanOrEqual(high_limit);
-    });
+    for (let i = 0; i < 100; i++) {
+      ROLL_DICE_TESTS.forEach(({ input, output_range }) => {
+        const [low_limit, high_limit] = output_range;
+        const output = rollDice(input);
+        expect(output).toBeGreaterThanOrEqual(low_limit);
+        expect(output).toBeLessThanOrEqual(high_limit);
+      });
+    }
+  });
+
+  test("it should throw an error for invalid dice notation", () => {
+    expect(() => rollDice("1d6+2d6+3d")).toThrowError("Invalid dice notation");
+    expect(() => rollDice("1d6+2d6+")).toThrowError("Invalid dice notation");
+    expect(() => rollDice("1")).toThrowError("Invalid dice notation");
+    expect(() => rollDice("1dd2")).toThrowError("Invalid dice notation");
+  });
+});
+
+describe("rollSingleDice", () => {
+  test("it should roll a single dice", () => {
+    for (let i = 0; i < 100; i++) {
+      const output = rollSingleDice(6);
+      expect(output).toBeGreaterThanOrEqual(1);
+      expect(output).toBeLessThanOrEqual(6);
+    }
   });
 });
--- src/293-roll-dice/roll-dice.ts
+++ src/293-roll-dice/roll-dice.ts
@@ -15,15 +15,22 @@ Example:
 ```
 */
 
-export const rollDice = (dice: string): number => {
-  if (dice === "4d4") {
-    return 13;
-  }
-  if (dice === "3d20") {
-    return 28;
-  }
-  if (dice === "1d8+2d10") {
-    return 21;
-  }
-  return 0;
+export const DICE_REGEX = /^(\d+d\d+)(\+\d+d\d+)*$/;
+
+export const rollSingleDice = (sides: number): number => {
+  return Math.floor(Math.random() * sides) + 1;
+};
+
+export const rollDice = (dices: string): number => {
+  if (!DICE_REGEX.test(dices)) throw new Error("Invalid dice notation");
+
+  return dices.split("+").reduce((sum, dice) => {
+    const [count, sides] = dice.split("d").map(s => Number(s));
+    return (
+      sum +
+      Array(count)
+        .fill(0)
+        .reduce(sum => sum + rollSingleDice(sides), 0)
+    );
+  }, 0);
 };
